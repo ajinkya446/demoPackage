079e795acb7469cea84927709b1b1cd16915af25
Ajinkya Aher <Ajinkya446@gmail.com>
2021-02-11 22:24:18 +0530

    first commit

---
+++ package_create/lib/creation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreationPackage extends StatefulWidget {
+  @override
+  _CreationPackageState createState() => _CreationPackageState();
+}
+
+class _CreationPackageState extends State<CreationPackage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
