d680451e0047fd9c1879f9fc99a51423eab2b8c6
Ajinkya Aher <Ajinkya446@gmail.com>
2021-02-11 23:15:39 +0530

    second commit

--- package_create/lib/creation.dart
+++ package_create/lib/creation.dart
@@ -9,11 +9,11 @@ class _CreationPackageState extends State<CreationPackage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child:Text("Demo")
     );
   }
 
-  getDetails(String data){
-    return data;
-  }
+  // getDetails(String data){
+  //   return data;
+  // }
 }
